3f1e0ca4a55021cc0e5e17e169ee794dd4fedd2a
Sahana Tankala <sahanatankala@gmail.com>
2021-11-04 13:09:46 -0400

    update symlinks

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-ln -s $HOME/dotfiles/alacritty $HOME/.config/alacritty
 ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
-ln -s $HOME/dotfiles/doom $HOME/.config/doom
 ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/omz $HOME/.config/omz
 cp -r $HOME/dotfiles/monalisa $HOME/.local/share/fonts/monolisa
