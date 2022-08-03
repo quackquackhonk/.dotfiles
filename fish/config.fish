4805a3c4eaa67e5c72ce0212b5d8753c98c8a39d
Sahana Tankala <sahanatankala@gmail.com>
2022-08-03 13:20:39 -0400

    Update fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    #
+    alias gs='git status'
+    alias cdc='cd /mnt/c/'
+    alias cd~='cd /mnt/c/Users/sahan'
+    alias cdd='cd /mnt/d/'
+    alias cde='cd /mnt/f/'
+    alias cdcode='cd ~/code'
+    alias nv='nvim'
+    alias vi='nvim'
+    export TERM=screen-256color
 end
