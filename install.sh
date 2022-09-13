2a62937ebd7228d58be488707906340dc4a8ca77
Sahana Tankala <sahanatankala@gmail.com>
2022-09-12 22:21:41 -0400

    update install script

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ ln -s $HOME/dotfiles/doom $HOME/.doom.d
 ln -s $HOME/dotfiles/fish $HOME/.config/
 ln -s $HOME/dotfiles/bacon $HOME/.config/
 ln -s $HOME/dotfiles/.tmux.conf $HOME/
+ln -s $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/zsh/sahana.zsh-theme $HOME/.oh-my-zsh/themes/
