e65722a6e6fb539e2e13722c98e3eb9af072763b
Sahana Tankala <sahanatankala@gmail.com>
2022-11-27 18:19:24 -0500

    emacs stuff

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ln -s $HOME/.dotfiles/nvim $HOME/.config/
 ln -s $HOME/.dotfiles/alacritty $HOME/.config/
-ln -s $HOME/.dotfiles/doom $HOME/.doom.d
+ln -s $HOME/.dotfiles/emacs/init.el $HOME/.emacs.d/
 ln -s $HOME/.dotfiles/fish $HOME/.config/
 ln -s $HOME/.dotfiles/bacon $HOME/.config/
 ln -s $HOME/.dotfiles/.tmux.conf $HOME/
