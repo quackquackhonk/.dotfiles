e198635a166f728d8aae4d7fb967c737d3fb5b1a
Sahana Tankala <sahanatankala@gmail.com>
2022-12-16 13:54:35 -0500

    switch to helm

--- emacs/init.el
+++ emacs/init.el
@@ -8,8 +8,9 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" default))
+ '(helm-minibuffer-history-key "M-p")
  '(package-selected-packages
-   '(evil-snipe counsel swiper company evil-surround evil-numbers evil use-package rainbow-delimiters ivy doom-themes doom-modeline command-log-mode))
+   '(helm-rg helm-xref helm-lsp helm evil-snipe counsel swiper company evil-surround evil-numbers evil use-package rainbow-delimiters ivy doom-themes doom-modeline command-log-mode))
  '(warning-suppress-log-types '((use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
