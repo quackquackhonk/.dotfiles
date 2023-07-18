7491d4f54c73e64992c8391ecfde36d0ed7b6516
Sahana Tankala <sahanatankala@gmail.com>
2023-07-18 12:15:56 -0400

    More neogit :)

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -98,7 +98,6 @@ require("lazy").setup({
         },
         config = function ()
             require('neogit').setup {
-                kind = "split_above",
                 use_magit_keybindings = true,
                 disable_insert_on_commit = false,
             }
