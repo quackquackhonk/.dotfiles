11518d4c4caa38fb115d6c85ae0d3012a49e8c18
Sahana Tankala <sahana.tankala@verisk.com>
2023-10-11 16:44:28 +0000

    idk

--- install_wsl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# cp -r $HOME/dotfiles/nvim /mnt/c/Users/sahan/AppData/Local/
-mkdir /mnt/c/Users/sahan/AppData/Roaming/alacritty
-cp $HOME/.dotfiles/alacritty/wsl.yml  /mnt/c/Users/sahan/AppData/Roaming/alacritty/alacritty.yml
-cp $HOME/.dotfiles/emacs/init.el /mnt/c/msys64/home/sahan/.emacs.d/init.el
-cp $HOME/.dotfiles/emacs/config.org /mnt/c/msys64/home/sahan/.emacs.d/config.org
