f3b35e13fe545a4717da2e77d5eece964b361090
Sahana Tankala <sahanatankala@gmail.com>
2024-01-17 18:52:55 -0500

    WEEEEEE

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -137,241 +137,123 @@ require("ibl").setup { scope = { highlight = highlight } }
 
 hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
 
+-- https://github.com/nvim-lualine/lualine.nvim/blob/master/examples/bubbles.lua
+-- Bubbles config for lualine
+-- Author: lokesh-krishna
+-- MIT license, see LICENSE for more details.
 
-require('tabby.tabline').use_preset('tab_only', {
-    theme = {
-        fill = 'TabLineFill',
-        head = 'TabLineEdge',
-        current_tab = 'TabLineSel',
-        tab = 'TabLine',
-        win = 'TabLine',
-        tail = 'TabLineEdge',
+colors.blue         = colors.neutral_blue
+colors.cyan         = colors.neutral_aqua
+colors.black        = colors.dark0_hard
+colors.white        = colors.light1
+colors.red          = colors.bright_red
+colors.violet       = colors.neutral_purple
+colors.grey         = colors.dark0_soft
+colors.green        = colors.neutral_green
+colors.orange       = colors.neutral_orange
+
+local bubbles_theme = {
+    normal = {
+        a = { fg = colors.black, bg = colors.blue },
+        b = { fg = colors.white, bg = colors.grey },
+        c = { fg = colors.gray, bg = colors.black },
     },
-    nerdfont = true, -- whether use nerdfont
-    buf_name = {
-        mode = "'unique'|'relative'|'tail'|'shorten'",
+
+    insert = { a = { fg = colors.black, bg = colors.green } },
+    command = { a = { fg = colors.black, bg = colors.orange } },
+    visual = { a = { fg = colors.black, bg = colors.violet } },
+    replace = { a = { fg = colors.black, bg = colors.red } },
+
+    inactive = {
+        a = { fg = colors.white, bg = colors.black },
+        b = { fg = colors.white, bg = colors.black },
+        c = { fg = colors.black, bg = colors.black },
     },
-
-})
-
--- Eviline config for lualine
--- Author: shadmansaleh
--- Credit: glepnir
-local lualine = require('lualine')
-
--- Color table for highlights
--- stylua: ignore
-local colors = {
-    bg       = colors.dark0,
-    fg       = colors.light0,
-    yellow   = colors.bright_yellow,
-    cyan     = colors.bright_aqua,
-    darkblue = colors.neutral_blue,
-    green    = colors.bright_green,
-    orange   = colors.bright_blue,
-    violet   = colors.bright_blue,
-    magenta  = colors.bright_blue,
-    blue     = colors.bright_blue,
-    red      = colors.bright_blue,
 }
 
-local conditions = {
-    buffer_not_empty = function()
-        return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
-    end,
-    hide_in_width = function()
-        return vim.fn.winwidth(0) > 80
-    end,
-    check_git_workspace = function()
-        local filepath = vim.fn.expand('%:p:h')
-        local gitdir = vim.fn.finddir('.git', filepath .. ';')
-        return gitdir and #gitdir > 0 and #gitdir < #filepath
-    end,
-}
-
--- Config
-local config = {
+require('lualine').setup({
     options = {
-        -- Disable sections and component separators
-        component_separators = '',
-        section_separators = '',
-        theme = {
-            -- We are going to use lualine_c an lualine_x as left and
-            -- right section. Both are highlighted by c theme .  So we
-            -- are just setting default looks o statusline
-            normal = { c = { fg = colors.fg, bg = colors.bg } },
-            inactive = { c = { fg = colors.fg, bg = colors.bg } },
-        },
+        theme = bubbles_theme,
+        component_separators = '|',
+        section_separators = { left = '', right = '' },
     },
     sections = {
-        -- these are to remove the defaults
-        lualine_a = {},
-        lualine_b = {},
-        lualine_y = {},
-        lualine_z = {},
-        -- These will be filled later
-        lualine_c = {},
-        lualine_x = {},
+        lualine_a = {
+            { 'mode', separator = { left = '' }, right_padding = 2 },
+        },
+        lualine_b = { 'filename', 'branch' },
+        lualine_c = { 'fileformat' },
+        lualine_x = {
+            {
+                require("noice").api.statusline.mode.get,
+                cond = require("noice").api.statusline.mode.has,
+                color = { fg = colors.gray },
+            }
+        },
+        lualine_y = { 'filetype', 'progress' },
+        lualine_z = {
+            { 'location', separator = { right = '' }, left_padding = 2 },
+        },
     },
     inactive_sections = {
-        -- these are to remove the defaults
-        lualine_a = {},
+        lualine_a = { 'filename' },
         lualine_b = {},
-        lualine_y = {},
-        lualine_z = {},
         lualine_c = {},
         lualine_x = {},
+        lualine_y = {},
+        lualine_z = { 'location' },
     },
-}
+    extensions = {},
+})
 
--- Inserts a component in lualine_c at left section
-local function ins_left(component)
-    table.insert(config.sections.lualine_c, component)
+local tabby = function()
+    --- https://github.com/nanozuki/tabby.nvim/blob/main/lua/tabby/presets.lua
+    local util = require('tabby.util')
+
+    local hl_tabline_fill = util.extract_nvim_hl('lualine_c_normal')
+    local hl_tabline = util.extract_nvim_hl('lualine_b_normal')
+    local hl_tabline_sel = util.extract_nvim_hl('lualine_a_normal')
+    local hl_inactive = { fg = colors.light0, bg = colors.dark1 }
+
+    local function tab_label(tabid, active)
+        local icon = active and '' or ''
+        local number = vim.api.nvim_tabpage_get_number(tabid)
+        local name = util.get_tab_name(tabid)
+        return string.format(' %s %d: %s ', icon, number, name)
+    end
+
+    local presets = {
+        hl = 'lualine_c_normal',
+        layout = 'tab_only',
+        head = {
+            { ' 󰣐 ', hl = { fg = hl_tabline.fg, bg = hl_tabline.bg } },
+            { '', hl = { fg = hl_tabline.bg, bg = hl_tabline_fill.bg } },
+        },
+        active_tab = {
+            label = function(tabid)
+                return {
+                    tab_label(tabid, true),
+                    hl = { fg = hl_tabline_sel.fg, bg = hl_tabline_sel.bg, style = 'bold' },
+                }
+            end,
+            left_sep = { ' ', hl = { fg = hl_tabline_sel.bg, bg = hl_tabline_fill.bg } },
+            right_sep = { '', hl = { fg = hl_tabline_sel.bg, bg = hl_tabline_fill.bg } },
+        },
+        inactive_tab = {
+            label = function(tabid)
+                return {
+                    tab_label(tabid, false),
+                    hl = hl_inactive,
+                }
+            end,
+            left_sep = { ' ', hl = { fg = hl_inactive.bg, bg = hl_tabline_fill.bg } },
+            right_sep = { '', hl = { fg = hl_inactive.bg, bg = hl_tabline_fill.bg } },
+        },
+    }
+
+    return presets
 end
 
--- Inserts a component in lualine_x at right section
-local function ins_right(component)
-    table.insert(config.sections.lualine_x, component)
-end
-
-ins_left {
-    function()
-        return '▊'
-    end,
-    color = { fg = colors.blue },      -- Sets highlighting of component
-    padding = { left = 0, right = 1 }, -- We don't need space before this
-}
-
-ins_left {
-    -- mode component
-    function()
-        return ''
-    end,
-    color = function()
-        -- auto change color according to neovims mode
-        local mode_color = {
-            n = colors.red,
-            i = colors.green,
-            v = colors.blue,
-            [''] = colors.blue,
-            V = colors.blue,
-            c = colors.magenta,
-            no = colors.red,
-            s = colors.orange,
-            S = colors.orange,
-            [''] = colors.orange,
-            ic = colors.yellow,
-            R = colors.violet,
-            Rv = colors.violet,
-            cv = colors.red,
-            ce = colors.red,
-            r = colors.cyan,
-            rm = colors.cyan,
-            ['r?'] = colors.cyan,
-            ['!'] = colors.red,
-            t = colors.red,
-        }
-        return { fg = mode_color[vim.fn.mode()] }
-    end,
-    padding = { right = 1 },
-}
-
-ins_left {
-    -- filesize component
-    'filesize',
-    cond = conditions.buffer_not_empty,
-}
-
-ins_left {
-    'filename',
-    cond = conditions.buffer_not_empty,
-    color = { fg = colors.magenta, gui = 'bold' },
-}
-
-ins_left { 'location' }
-
-ins_left { 'progress', color = { fg = colors.fg, gui = 'bold' } }
-
-ins_left {
-    'diagnostics',
-    sources = { 'nvim_diagnostic' },
-    symbols = { error = ' ', warn = ' ', info = ' ' },
-    diagnostics_color = {
-        color_error = { fg = colors.red },
-        color_warn = { fg = colors.yellow },
-        color_info = { fg = colors.cyan },
-    },
-}
-
--- Insert mid section. You can make any number of sections in neovim :)
--- for lualine it's any number greater then 2
-ins_left {
-    function()
-        return '%='
-    end,
-}
-
-ins_left {
-    -- Lsp server name .
-    function()
-        local msg = 'No Active Lsp'
-        local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-        local clients = vim.lsp.get_active_clients()
-        if next(clients) == nil then
-            return msg
-        end
-        for _, client in ipairs(clients) do
-            local filetypes = client.config.filetypes
-            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                return client.name
-            end
-        end
-        return msg
-    end,
-    icon = ' LSP:',
-    color = { fg = colors.fg, gui = 'bold' },
-}
-
--- Add components to right sections
-ins_right {
-    'o:encoding',       -- option component same as &encoding in viml
-    fmt = string.upper, -- I'm not sure why it's upper case either ;)
-    cond = conditions.hide_in_width,
-    color = { fg = colors.green, gui = 'bold' },
-}
-
-ins_right {
-    'fileformat',
-    fmt = string.upper,
-    icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
-    color = { fg = colors.green, gui = 'bold' },
-}
-
-ins_right {
-    'branch',
-    icon = '',
-    color = { fg = colors.violet, gui = 'bold' },
-}
-
-ins_right {
-    'diff',
-    -- Is it me or the symbol for modified us really weird
-    symbols = { added = ' ', modified = '󰝤 ', removed = ' ' },
-    diff_color = {
-        added = { fg = colors.green },
-        modified = { fg = colors.orange },
-        removed = { fg = colors.red },
-    },
-    cond = conditions.hide_in_width,
-}
-
-ins_right {
-    function()
-        return '▊'
-    end,
-    color = { fg = colors.blue },
-    padding = { left = 1 },
-}
-
--- Now don't forget to initialize lualine
-lualine.setup(config)
+require('tabby').setup({
+    tabline = tabby()
+})
