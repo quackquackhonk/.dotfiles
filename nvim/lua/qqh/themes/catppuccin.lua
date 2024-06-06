b73df3590d3eadde802d77b85d49fb550686ee03
Sahana Tankala <sahana.tankala@verisk.com>
2024-06-06 16:09:35 -0400

    theme

--- nvim/lua/qqh/themes/catppuccin.lua
+++ nvim/lua/qqh/themes/catppuccin.lua
@@ -32,9 +32,9 @@ require("catppuccin").setup({
 			TabLineFill = { bg = colors.mantle },
 
 			-- Telescope
-			TelescopeNormal = { fg = colors.blue, bg = colors.mantle },
-			TelescopeBorder = { fg = colors.blue, bg = colors.mantle },
-			TelescopePromptBorder = { fg = colors.blue, bg = colors.mantle },
+			TelescopeNormal = { fg = colors.blue, bg = colors.crust },
+			TelescopeBorder = { fg = colors.blue, bg = colors.crust },
+			TelescopePromptBorder = { fg = colors.blue, bg = colors.crust },
 			TelescopeSelection = { bg = colors.base },
 			TelescopeMatching = { fg = colors.pink },
 
