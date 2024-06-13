5c119d3fc50ba642e7ee9a9e2e1c7fab5e472448
Sahana Tankala <sahanatankala@gmail.com>
2024-06-12 22:28:01 -0400

    finally, she's modular

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -24,19 +24,15 @@
       monstera = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         # > Our main nixos configuration file <
-        modules = [./nixos/configuration.nix];
-      };
-    };
+        modules = [
+          ./nixos/configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
 
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      # FIXME replace with your username@hostname
-      "sahana@monstera" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        # > Our main home-manager configuration file <
-        modules = [./home-manager/home.nix];
+            home-manager.users.sahana = import ./home-manager/home.nix;
+          }
+        ];
       };
     };
   };
