30a4ea0a3ed899dbe79b923c56ea56c76e1c4711
Sahana Tankala <sahanatankala@gmail.com>
2024-06-12 20:02:59 -0400

    adding light

--- nixos/hosts/monstera/configuration.nix
+++ nixos/hosts/monstera/configuration.nix
@@ -17,19 +17,25 @@
   networking.networkmanager.enable = true;
   time.timeZone = "US/Eastern";
   i18n.defaultLocale = "en_US.UTF-8";
+
+  programs.hyprland.enable = true;
+  programs.light.enable = true;
+
   services.printing.enable = true;
   services.pipewire = {
     enable = true;
     pulse.enable = true;
   };
   services.libinput.enable = true;
+  services.openssh.enable = true;
+  # networking.firewall.allowedTCPPorts = [ 22 ];
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   programs.fish.enable = true;
   users.defaultUserShell = pkgs.fish;
   users.users.sahana = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "networkmanager" "docker" ]; # Enable ‘sudo’ for the user.
   };
 
   home-manager = {
@@ -46,17 +52,12 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    gcc14 cmake
-    firefox alacritty
-    starship
-    fzf
-    zoxide
-    rustup
-    git
     vim
+    gcc
+    gnumake
+    gnupg
     wget
-    eza
-    zellij
+    curl
   ];
   
   programs.nh = {
@@ -65,36 +66,8 @@
     clean.extraArgs = "--keep-since 4d --keep 3";
     flake = "/home/sahana/nixos";
   };
-  programs.hyprland.enable = true;
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  # Never ever change this
   system.stateVersion = "24.05"; # Did you read the comment?
 
 }
--- nixos/hosts/monstera/home.nix
+++ nixos/hosts/monstera/home.nix
@@ -5,31 +5,27 @@
   # manage.
   home.username = "sahana";
   home.homeDirectory = "/home/sahana";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  programs.git = {
+    package = pkgs.gitAndTools.gitFull;
+    enable = true;
+    userName = "Sahana Tankala";
+    userEmail = "sahanatankala@gmail.com";
+  };
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+  home.packages = with pkgs; [
+    firefox 
+    alacritty
+    starship
+    fzf
+    zoxide
+    rustup
+    eza
+    zellij
+    ripgrep
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -47,24 +43,8 @@
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/sahana/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
   };
 
   # Let Home Manager install and manage itself.
