daf533283f3fa2d38352fdc15483e7a2f8853d62
Sahana Tankala <sahana.tankala@verisk.com>
2025-07-11 13:15:53 -0400

    Evil mode port complete!

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1133,8 +1133,9 @@ back to `consult-fd' if we're not in a project."
   (interactive)
   (find-file "~/dotfiles/home/hyprland.nix"))
 
-;;;; surround: surround selections with custom delimiters
-(use-package surround)
+;;;; Undo-fu
+(use-package undo-fu
+  :straight (undo-fu :type git :host github :repo "emacsmirror/undo-fu"))
 
 ;;;; transient: so many leader keys
 (use-package transient
@@ -1197,7 +1198,7 @@ back to `consult-fd' if we're not in a project."
 These bindings are preferred over `meow-leader-define-key', since I have less restrictions here!"
   ["leader bindings..."
    ("SPC" "buffers" consult-buffer)
-   ("," "last buffer" meow-last-buffer)
+   ("," "last buffer" evil-switch-to-windows-last-buffer)
    (":" "eval expression" eval-expression)]
   [:class transient-row
           ("c" "+code" qqh/transient/code)
@@ -1206,21 +1207,6 @@ These bindings are preferred over `meow-leader-define-key', since I have less re
           ("n" "+notes" qqh/transient/notes)
           ("s" "+search" qqh/transient/search)
           (";" "+config" qqh/transient/config)])
-
-(transient-define-prefix qqh/transient/g ()
-  "Transient map for emulating vim's g- leader keybinding."
-  [["Edit"
-    ("SPC" "code action" eglot-code-actions :if (lambda () (and (featurep 'eglot) eglot--managed-mode)))
-    ("R" "rename symbol" eglot-rename :if (lambda () (and (featurep 'eglot) eglot--managed-mode)))
-    ("c" "comment" comment-dwim)]
-   ["Find"
-    ("d" "definition" xref-find-definitions)
-    ("r" "references" xref-find-references)]
-   ["Move"
-    ("g" "top" beginning-of-buffer)
-    ("G" "bottom" end-of-buffer)
-    ("l" "line" meow-goto-line)
-    ("RET" "2 chars" avy-goto-char-2)]])
 
 (transient-define-prefix qqh/transient/next ()
   "Transient map for going to the next thing."
@@ -1246,176 +1232,83 @@ These bindings are preferred over `meow-leader-define-key', since I have less re
 
 (global-set-key (kbd "M-<mouse-1>") 'goto-address-at-mouse)
 
-;;;; Meow
+(global-set-key (kbd "M-q") 'qqh/kill-buffer)
+(global-set-key (kbd "C-q") 'evil-window-delete)
 
-(use-package meow
-  :straight t
-  :demand t
-  :custom-face
-  ;; mode line faces
-  (meow-normal-indicator ((t (:bold t :foreground ,(catppuccin-color 'base) :background ,(catppuccin-color 'mauve)))))
-  (meow-normal-cursor ((t :inherit meow-normal-indicator)))
-  (meow-motion-indicator ((t (:bold t :foreground ,(catppuccin-color 'base) :background ,(catppuccin-color 'red)))))
-  (meow-motion-cursor ((t :inherit meow-motion-indicator)))
-  (meow-keypad-indicator ((t (:bold t :foreground ,(catppuccin-color 'base) :background ,(catppuccin-color 'peach)))))
-  (meow-keypad-cursor ((t :inherit meow-keypad-indicator)))
-  (meow-insert-indicator ((t (:bold t :foreground ,(catppuccin-color 'base) :background ,(catppuccin-color 'green)))))
-  (meow-insert-cursor ((t :inherit meow-insert-indicator)))
-  (meow-beacon-indicator ((t (:bold t :foreground ,(catppuccin-color 'base) :background ,(catppuccin-color 'sapphire)))))
-  (meow-beacon-cursor ((t :inherit meow-normal-indicator))))
+;;;; Evil mode
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-respect-visual-line-mode t)
+  ;; settings
+  (setq evil-want-keybinding nil
+        evil-want-unimpaired-p nil
+        evil-want-integration t
+        evil-want-C-i-jump nil
+        evil-shift-width 2)
+  :config
+  ;; enable the stuff
+  (evil-set-undo-system 'undo-fu)
+  (evil-mode 1)
 
-(defun meow-setup ()
-  "Function for setting up meow keybindings."
-  ;; colemak-dh cheatsheet
-  (setq meow-cheatsheet-layout meow-cheatsheet-layout-colemak-dh)
+  ;; Configuring initial major mode for some modes
+  (evil-set-initial-state 'eat-mode 'emacs)
+  (evil-set-initial-state 'vterm-mode 'emacs)
+  (add-hook 'git-commit-setup-hook 'evil-insert-state)
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
 
-  ;; all modes use a box cursor
-  (setq meow-cursor-type-default 'box)
-  (setq meow-cursor-type-normal 'box)
-  (setq meow-cursor-type-motion 'box)
-  (setq meow-cursor-type-insert 'box)
-  (setq meow-cursor-type-keypad 'hollow)
+;;;;; Evil plugins
+(use-package evil-collection
+  :after evil
+  :custom
+  (evil-collection-want-unimpaired-p nil)
+  (evil-collection-magit-setup t)
+  (evil-collection-forge-setup t)
+  :config
+  (evil-collection-init))
 
-  ;; register some things
-  (meow-thing-register 'angle
-                       '(pair ("<") (">"))
-                       '(pair ("<") (">")))
+(use-package evil-commentary
+  :after evil
+  :config
+  (evil-commentary-mode))
 
-  ;; sets the thing table characters to use ([{ for grouping punctuations
-  ;; test ' asrtarstar '
-  (setq meow-char-thing-table
-        '((?\( . round)
-          (?\) . round)
-          (?\[ . square)
-          (?\] . square)
-          (?\{ . curly)
-          (?\} . curly)
-          (?\< . angle)
-          (?\> . angle)
-          (?\" . string)
-          (?s . symbol)
-          (?w . window)
-          (?b . buffer)
-          (?b . paragraph)
-          (?l . line)
-          (?v . visual-line)
-          (?f . defun)
-          (?\. . sentence)))
+(use-package evil-surround
+  :config
+  (global-evil-surround-mode 1))
 
-  (meow-motion-overwrite-define-key
-   ;; Use e to move up, n to move down.
-   ;; Since special modes usually use n to move down, we only overwrite e here.
-   '("e" . meow-prev)
-   '(":" . meow-keypad)
-   '("[" . qqh/transient/prev)
-   '("]" . qqh/transient/next)
-   '("SPC" . qqh/transient/leader)
-   '("<escape>" . ignore))
+(use-package evil-snipe
+  :after evil
+  :config
+  (evil-snipe-mode +1)
+  (evil-snipe-override-mode +1))
 
-  ;; default meow leader bindings
-  (meow-leader-define-key
-   ;; To execute the originally e in MOTION state, use SPC e.
-   '("e" . "H-e")
-   '("1" . meow-digit-argument)
-   '("2" . meow-digit-argument)
-   '("3" . meow-digit-argument)
-   '("4" . meow-digit-argument)
-   '("5" . meow-digit-argument)
-   '("6" . meow-digit-argument)
-   '("7" . meow-digit-argument)
-   '("8" . meow-digit-argument)
-   '("9" . meow-digit-argument)
-   '("0" . meow-digit-argument))
+(use-package goto-chg
+  :after evil)
 
-  (meow-normal-define-key
-   '("0" . meow-expand-0)
-   '("1" . meow-expand-1)
-   '("2" . meow-expand-2)
-   '("3" . meow-expand-3)
-   '("4" . meow-expand-4)
-   '("5" . meow-expand-5)
-   '("6" . meow-expand-6)
-   '("7" . meow-expand-7)
-   '("8" . meow-expand-8)
-   '("9" . meow-expand-9)
-   '("a" . meow-append)
-   '("A" . ignore)
-   '("b" . meow-back-word)
-   '("B" . meow-back-symbol)
-   '("c" . meow-change)
-   '("C" . "C-c")
-   '("d" . meow-kill)
-   '("e" . meow-prev)
-   '("E" . meow-prev-expand)
-   '("f" . meow-find)
-   '("G" . meow-grab)
-   '("m" . meow-left)
-   '("M" . meow-left-expand)
-   '("i" . meow-right)
-   '("I" . meow-right-expand)
-   '("j" . meow-join)
-   '("k" . ignore)
-   '("K" . eldoc)
-   '("l" . meow-line)
-   '("L" . avy-goto-line)
-   '("h" . meow-mark-word)
-   '("H" . meow-mark-symbol)
-   '("n" . meow-next)
-   '("N" . meow-next-expand)
-   '("o" . meow-open-below)
-   '("O" . meow-open-above)
-   (cons "P" projectile-command-map)
-   '("p" . meow-yank)
-   '("q" . meow-quit)
-   '("r" . meow-replace)
-   '("s" . meow-insert)
-   (cons "S" surround-keymap)
-   '("t" . meow-till)
-   '("u" . meow-undo)
-   '("U" . meow-undo-in-selection)
-   '("v" . meow-search)
-   '("w" . meow-next-word)
-   '("W" . meow-next-symbol)
-   '("x" . meow-delete)
-   '("X" . "C-x")
-   '("y" . meow-save)
-   '("z" . meow-pop-selection)
-   '("-" . negative-argument)
-   '(";" . meow-reverse)
-   '("," . meow-inner-of-thing)
-   '("." . meow-bounds-of-thing)
-   '("?" . "C-h")
-   '("(" . meow-beginning-of-thing)
-   '(")" . meow-end-of-thing)
-   '("/" . meow-visit)
-   '("'" . repeat)
-   '("<" . meow-pop-to-mark)
-   '(">" . meow-unpop-to-mark)
-   '("<escape>" . keyboard-quit)
-   '("RET" . ignore)
-   '("SPC" . qqh/transient/leader)
+;;;; Evil Bindings
 
-   ;; Some vim-like bindings
-   '("g" . qqh/transient/g)
-   '(":" . meow-keypad)
-   '("="   . meow-indent)
-   ;; TODO: $ for end of line
-   '("C-q" . delete-window)
-   '("M-q" . qqh/kill-buffer)
+;; Setup my transients and maps
+(define-key evil-normal-state-map (kbd "SPC") 'qqh/transient/leader)
+(define-key evil-normal-state-map (kbd "P") 'projectile-command-map)
+(define-key evil-normal-state-map (kbd "M-q") 'qqh/kill-buffer)
 
-   ;; Bracketed movement
-   '("[" . qqh/transient/prev)
-   '("]" . qqh/transient/next)
+;; Customize the g keymap
+(define-key evil-normal-state-map (kbd "L") 'avy-goto-line)
+(define-key evil-normal-state-map (kbd "g RET") 'avy-goto-char-2)
+;; Eglot bindings
+(add-hook 'eglot-connect-hook
+          (lambda ()
+            (define-key evil-normal-state-map (kbd "g R") 'eglot-rename)
+            (define-key evil-normal-state-map (kbd "g SPC") 'eglot-code-actions)))
 
-   ;; embark keybindings
-   '("C-." . embark-act)         ;; pick some comfortable binding
-   '("C-;" . embark-dwim)        ;; good alternative: M-.
-   '("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
-  )
+;; Add my own bracketed movement options
+(define-key evil-motion-state-map (kbd "[ e") 'flymake-goto-prev-error)
+(define-key evil-motion-state-map (kbd "] e") 'flymake-goto-next-error)
+(define-key evil-motion-state-map (kbd "] x") 'smerge-vc-next-conflict)
 
-(require 'meow)
-(meow-setup)
-(meow-global-mode 1)
+;; C-g quits normal mode
+(define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
 
 ;;; Customization file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
